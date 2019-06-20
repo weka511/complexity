97b8531c7b4275f7906ab3f278ffc806e85a1e13
Simon Crase <simon@greenweaves.nz>
2019-06-21 08:08:29 +1200

    Issue #1: harmonized notation with lecture notes

--- origins/error_catastrophe.py
+++ origins/error_catastrophe.py
@@ -1,25 +1,32 @@
-def evolve(A,replication=1.0,advantage=9,q=0.88):
+from numpy import isclose
+
+def evolve(A,s=0.01,nu=0.01):
     
     def grow(i,a):
-        return a * replication * (advantage if i==0 else 1)
-
-    def mutations(i):
-        unmutated  = q * A1[i]
-        from_lower = 0 if i==0 else A1[i-1] * (1-q) * (n-i+1) / n # n-i+1 zeroes in i-1
-        from_upper = 0 if i==n else A1[i+1] * (1-q) * (i+1) / n # i+1 ones in i+1
-        return from_lower + unmutated + from_upper  
+        return a * (1+s) if i ==0 else 0
     
-    n     = len(A) - 1
+    L     = len(A) - 1
     A1    = [grow(i,A[i]) for i in range(len(A)) ]
-    A2    = [mutations(i) for i in range(len(A1))]
+    A2    = (len(A)) * [0]
+    for i in range(len(A1)):
+        A2[i]     += (1 - L * nu) * A1[i]
+        #n_mutants = (1-q) * A1[i]
+        #n_ones    = i
+        #n_zeroes  = len(A1) - n_ones
+        if i>0:
+            A2[i-1] += (L-i) * nu
+        if i<len(A1)-1:
+            A2[i+1] += i * nu
+    
+    #assert isclose(sum(A1),sum(A2),rtol=1e-5),'Number should be conserved!'       
     total = sum(A2)
     return [a/total for a in A2]
 
 if __name__=='__main__':
-    M = 1000
-    N = 20
-    A = [1] + N * [0]
+    M = 10
+    L = 10
+    A = [1] + L * [0]
+    print (A)
     for i in range(M):
         A = evolve(A)
-        if (i%100==0):
-            print (A)
+        print (A)
