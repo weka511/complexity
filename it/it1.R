922a5d3d1513c736da8006c8bf484af629a11ce3
Simon Crase <simon@greenweaves.nz>
2019-09-26 11:21:33 +1200

    Calculate entropies for X and Y

--- it/it1.R
+++ it/it1.R
@@ -1,4 +1,15 @@
 if (!require('entropy')){
   install.packages('entropy')
-  library(entropy)
+  library('entropy')
 }
+
+my.path  <- '~/../complexity/it'
+raw.data <- read.csv(file.path(my.path,'santafe-temps.csv'))
+X        <- unlist(lapply(raw.data$MaxTemp,function(t) {return (if (t>=80) 1 else 0)}))
+Y        <- unlist(lapply(raw.data$Precipation,function(p) {return (if(p>0) 1 else 0)}))
+
+print (sprintf('I(X)=%.3f,I(X)=%.3f',
+               entropy(discretize(X,numBins = 2),unit='log2'),
+              entropy(discretize(Y,numBins = 2),unit='log2')))
+XY = discretize2d(X,Y,numBins1 = 2,numBins2 = 2)
+print(entropy(XY,unit='log2'))
