90a3b006af9329a832f340d7257561a907c88e51
Simon Crase <simon@greenweaves.nz>
2019-12-11 18:22:22 +1300

    Created program to plot Mexican hat

--- tm/plot-quartic.py
+++ tm/plot-quartic.py
@@ -1,3 +1,15 @@
+# Copyright (C) 2019 Greenweaves Software Limited
+
+# This is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
 import sys
 sys.path.append('../')
 
@@ -8,27 +20,23 @@ from matplotlib import cm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import numpy as np
 
-a = 0.1
-b = 0.1
-fig = plt.figure()
-ax = fig.gca(projection='3d')
+a    = 0.12
+b    = 0.1
+L    = 1.1
+fig  = plt.figure()
+ax   = fig.gca(projection='3d')
 
-# Make data.
-X = np.arange(-1, 1, 0.05)
-Y = np.arange(-1, 1, 0.05)
+X    = np.arange(-L, L, 0.05)
+Y    = np.arange(-L, L, 0.05)
 X, Y = np.meshgrid(X, Y)
-R = np.sqrt(X**2 + Y**2)
-#Z = np.sin(R)
-Z = -a*R + b *R *R
-# Plot the surface.
-surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
-                       linewidth=0, antialiased=False,alpha=0.5)
-# Customize the z axis.
-#ax.set_zlim(-1.01, 1.01)
-#ax.zaxis.set_major_locator(LinearLocator(10))
-#ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+R    = np.sqrt(X**2 + Y**2)
 
-# Add a color bar which maps values to colors.
-fig.colorbar(surf, shrink=0.5, aspect=5)
+Z    = -a*R + b *R *R
+
+ax.contour3D(X, Y, Z, 50, cmap=cm.coolwarm)
+ax.set_xticklabels([])
+ax.set_yticklabels([])
+ax.set_zticklabels([])
+plt.savefig('figs/mexican-hat')
 
 plt.show()
