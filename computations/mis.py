4719579f7e1b76f4771091294562e41144580e18
Simon Crase <simon@greenweaves.nz>
2020-07-29 20:07:10 +1200

    Added reference implementation

---
+++ computations/mis.py
@@ -0,0 +1,33 @@
+
+def mis(T):
+    unprocessed = []
+    score       = {}
+    weights  = {}
+    children = {}
+    def build_tree(T):
+        if len(T)==0:
+            return None
+        else:
+            Head = T[0]
+            Tail = T[1:]
+            node = len(weights)
+            weights[node] = T[0]
+            children [node] = []
+            unprocessed.append(node)
+            for rest in T[1:]:
+                nn = build_tree(rest)
+                if nn != None:
+                    children [node].append(nn)
+            return node
+    build_tree(T)
+ 
+    while len(unprocessed)>0:
+        node = unprocessed.pop()
+        if len(children[node])==0:
+            score[node]=weights[node]
+        else:
+            score[node] = max(sum([score[child] for child in children[node]]),
+                              weights[node]+sum([score[grandchild] for child in children[node] for grandchild in children[child]]))
+    return score[0]
+    
+print (mis([3,[4,[1],[2]],[1,[2]],[5,[1],[1]]]))
