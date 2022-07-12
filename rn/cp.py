98d2cff8702b3f161e22448cdb4669306d0f1604
Simon Crase <simon@greenweaves.nz>
2022-07-13 11:27:45 +1200

    Tidied up

--- rn/cp.py
+++ rn/cp.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2019 Greenweaves Software Limited
+# Copyright (C) 2019, 2022 Greenweaves Software Limited
 
 # This is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -13,21 +13,22 @@
 # You should have received a copy of the GNU General Public License
 # along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
 
-import matplotlib.pyplot as plt, numpy as np,math
+from matplotlib.pyplot import plot, scatter, text, xlabel, legend, savefig, show
+from numpy             import log, cosh, linspace, tanh
 
 def S(beta):
-    return 3*math.log(math.cosh(4*beta))/8
+    return 3*log(cosh(4*beta))/8
 
-def plot(beta,S_beta):
-    betas = np.linspace(0,1,100)
-    plt.plot(betas,[S(beta) for beta in betas],'b',label=r'$\frac{3}{8} \log(\cosh(4\beta))$')
-    plt.plot(betas,betas,'r',label=r'$\beta$')
-    plt.plot(betas,[3*math.tanh(4*beta)/2 for beta in betas],'k',ls='-.',label=r'Derivative: $\frac{3}{2} \tanh(4\beta)$')
-    plt.scatter(beta,S_beta)
-    plt.text(beta+0.01,S_beta+0.01,'Critical point {0:.6f}'.format(beta))
-    plt.xlabel(r'$\beta$')
-    plt.legend(loc='lower right')
-    plt.savefig('figs/beta.jpg')
+def plot_results(beta,S_beta):
+    betas = linspace(0,1,100)
+    plot(betas,[S(beta) for beta in betas],'b',label=r'$\frac{3}{8} \log(\cosh(4\beta))$')
+    plot(betas,betas,'r',label=r'$\beta$')
+    plot(betas,[3*tanh(4*beta)/2 for beta in betas],'k',ls='-.',label=r'Derivative: $\frac{3}{2} \tanh(4\beta)$')
+    scatter(beta,S_beta)
+    text(beta+0.01,S_beta+0.01,'Critical point {0:.6f}'.format(beta))
+    xlabel(r'$\beta$')
+    legend(loc='lower right')
+    savefig('figs/beta.jpg')
 
 def solve(eps=0.1):
     beta0  = 0
@@ -35,7 +36,7 @@ def solve(eps=0.1):
     beta1  = 1
     S1     = S(beta1)
     beta   = 0.5* (beta0+beta1)
-    S_beta = S(beta)    
+    S_beta = S(beta)
     while beta1-beta0>eps:
         if S_beta<beta:
             beta0 = beta
@@ -45,11 +46,11 @@ def solve(eps=0.1):
             S1    = S_beta
         beta   = 0.5* (beta0+beta1)
         S_beta = S(beta)
-        
+
     return (beta,S_beta)
 
 if __name__=='__main__':
     beta,S_beta = solve(eps=1e-6)
-    plot(beta,S_beta)
-    
-    plt.show()
+    plot_results(beta,S_beta)
+
+    show()
