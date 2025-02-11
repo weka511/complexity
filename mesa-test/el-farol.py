c9fc3e12243504c4f8b8cea772060c461ac34f6f
Simon Crase <simon@greenweaves.nz>
2025-02-11 16:42:37 +1300

    #25 Support miltiple strategies

--- mesa-test/el-farol.py
+++ mesa-test/el-farol.py
@@ -15,7 +15,13 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-'''El Farol simulation--https://sites.santafe.edu/~wbarthur/Papers/El_Farol.pdf'''
+'''
+   El Farol simulation
+
+   Inductive Reasoning and Bounded Rationality, Brian Arthur,
+
+   https://sites.santafe.edu/~wbarthur/Papers/El_Farol.pdf
+'''
 
 from abc import ABC, abstractmethod
 from argparse import ArgumentParser
@@ -30,15 +36,15 @@ import pandas as pd
 
 def parse_arguments():
     parser = ArgumentParser(__doc__)
-    capacity = 70
+    capacity = 60
     population = 100
     iterations = 52
     parser.add_argument('--seed',type=int,default=None,help='Seed for random number generator')
     parser.add_argument('--figs', default = './figs',help='Path for storing figures')
     parser.add_argument('--show',default=False,action='store_true',help='Show plots')
-    parser.add_argument('--capacity', default=70, type=int,help = f'Capacity of venue[{capacity}]')
-    parser.add_argument('--population', default=1000, type=int,help = f'Number of people available to attend venue [{population}]')
-    parser.add_argument('--iterations', default=52, type=int, help = f'Number of iterations for running simulation [{iterations}]')
+    parser.add_argument('--capacity', default=capacity, type=int,help = f'Capacity of venue[{capacity}]')
+    parser.add_argument('--population', default=population, type=int,help = f'Number of people available to attend venue [{population}]')
+    parser.add_argument('--iterations', default=iterations, type=int, help = f'Number of iterations for running simulation [{iterations}]')
     return parser.parse_args()
 
 class PlotContext:
@@ -64,13 +70,15 @@ class PlotContext:
 
 class Strategy(ABC):
     '''An abstract class, whose implementation predict attendance'''
-    def __init__(self,random,population=100,log = []):
+    def __init__(self,random,population=100,log = [],m=1,name=''):
         self.random = random
         self.population = population
         self.log = log
+        self.m = m
+        self.name = name
 
     def get_predicted_attendance(self):
-        if len(self.log) == 0:
+        if len(self.log) < self.m:
             return self.random.random() * self.population
         else:
             return self.get_predicted()
@@ -79,39 +87,85 @@ class Strategy(ABC):
     def get_predicted(self):
         pass
 
-class YesterdaysWeather(Strategy):
-    '''
-    Predict attendance using a weighted average of the past few weeks.
-    '''
-    def __init__(self,random,population=100,log = [],n=4,a=0.75,b=0.25):
-        super().__init__(random,population,log)
-        self.a = a
-        self.b = b
-        self.n = n
+class MirrorImage(Strategy):
+    def __init__(self,random,population=100,log = []):
+        super().__init__(random,population,log,name='MirrorImage')
 
     def get_predicted(self):
-        return self.a * np.mean(self.log[-self.n:]) + self.b * self.random.random() * self.population
+        return self.population - log[-1]
+
+class Cycle(Strategy):
+    def __init__(self,random,population=100,log = [],m=3):
+        super().__init__(random,population,log,m=m,name=f'Cycle {m}')
+
+    def get_predicted(self):
+        return log[-self.m]
+
+class Average(Strategy):
+    def __init__(self,random,population=100,log = [],m=4):
+        super().__init__(random,population,log,name=f'Average {m}')
+
+    def get_predicted(self):
+        return  np.mean(self.log[-self.m:])
+
+class StrategyFactory:
+    def __init__(self,random):
+        self.random = random
+    def create(self,random,population,log):
+        match(self.random.randint(0,3-1)):
+            case 0:
+                return MirrorImage(random,population,log)
+            case 1:
+                return Cycle(random,population,log,m=random.randint(2,4))
+            case 2:
+                return Average(random,population,log,m=random.randint(2,4))
+
 
 class Patron(mesa.Agent):
+    '''
+    A bar Patron can decide whther or not to attend
+    '''
     def __init__(self,model):
         super().__init__(model)
         self.happiness = []
+        self.strategies = []
+        self.index = 0
 
     def decide(self):
-        self.attend = self.strategy.get_predicted_attendance() < self.capacity
+        self.attend = self.strategies[self.index].get_predicted_attendance() < self.capacity
 
+    def calculate_happiness(self,bar):
+        self.happiness.append(1 if self.attend and bar.is_comfortable() else 0)
+
+    def review_happiness(self,step_number,random):
+        if step_number>0 and step_number % 5 ==0:
+            if self.happiness[-1] < step_number * 0.1:
+                saved_index = self.index
+                while saved_index == self.index:
+                    self.index = random.randint(0,len(self.strategies)-1)
 
 class ElFarol(mesa.Model):
-    def __init__(self,population=100,seed=None,log=[]):
+    '''
+    The El Farol bar, which has a finite capacity
+    '''
+    def __init__(self,population=100,seed=None,log=[],capacity = 60):
         super().__init__(seed=seed)
         Patron.create_agents(model=self, n=population)
         self.log = log
+        self.capacity = capacity
 
-    def step(self):
+    def step(self,step_number):
         self.agents.shuffle_do('decide')
         self.log.append(sum(1 for agent in self.agents if agent.attend))
-        for agent in self.agents:
-            agent.happiness.append(1 if agent.attend and self.log[-1] < self.capacity else 0)
+        for patron in self.agents:
+            patron.calculate_happiness(self)
+            patron.review_happiness(step_number,self.random)
+
+    def is_comfortable(self):
+        '''
+        The bar is comfortable provided the attendance doesn't exceed capacity
+        '''
+        return self.log[-1] <= self.capacity
 
 
 if __name__=='__main__':
@@ -123,16 +177,18 @@ if __name__=='__main__':
     log = []
     bar = ElFarol(population = args.population,
                   seed = args.seed,
-                  log = log)
+                  log = log,
+                  capacity = args.capacity)
+    strategyfactory = StrategyFactory(bar.random)
 
-    strategy = YesterdaysWeather(bar.random,population=args.population,log=log)
-    bar.capacity = args.capacity
     for patron in bar.agents:
-        patron.strategy = strategy
+        for _ in range(5): #FIXME
+            patron.strategies.append(strategyfactory.create(bar.random,args.population,log))
         patron.capacity = args.capacity
 
-    for _ in range(args.iterations):
-        bar.step()
+
+    for step_number in range(args.iterations):
+        bar.step(step_number)
 
     happiness = [sum(agent.happiness) for agent in bar.agents]
     with PlotContext(nrows=2,ncols=1,figs=args.figs) as axes:
