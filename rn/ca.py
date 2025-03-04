2ebdf11f1881d36f17b817262edd01299ff82483
Simon Crase <simon@greenweaves.nz>
2025-03-05 10:00:35 +1300

    #35 Completed through to Q1 step 3

--- rn/ca.py
+++ rn/ca.py
@@ -39,16 +39,27 @@ class Rule:
     def __getitem__(self, state):
         return self.bits[state.n]
 
-    def execute(self,state):
-        extended_state = [0,0] + state.bits + [0,0]
-        new_state = []
-        for i in range(len(state)+2):
-            x = extended_state[i:i+3]
-            new_state.append(self.bits[4*x[0] + 2*x[1] + x[2]])
-        return new_state
+    def two_step(self,superstate):
+        if len(superstate) != 6: raise ValueError('superstate should have length 6')
+        supercell_bits = []
+        for i in range(2):
+            triplet = []
+            for j in range(3):
+                k = 3*i + j
+                state = State(bits=superstate[k:k+3])
+                triplet.append(self[state])
+            supercell_bits.append(self[State(bits=triplet)])
+            z=0
+    # def execute(self,state):
+        # extended_state = [0,0] + state.bits + [0,0]
+        # new_state = []
+        # for i in range(len(state)+2):
+            # x = extended_state[i:i+3]
+            # new_state.append(self.bits[4*x[0] + 2*x[1] + x[2]])
+        # return new_state
 
 class State:
-    def __init__(self,n):
+    def __init__(self,n=None,bits=None):
         '''
         Create a state
 
@@ -57,13 +68,22 @@ class State:
         Returns:
             A list of bits
         '''
-        m = n
-        result = []
-        for i in range(3):
-            m,r = divmod(m,2)
-            result.append(r)
-        self.bits = result[::-1]
-        self.n = n
+        if n != None and bits == None:
+            m = n
+            result = []
+            for i in range(3):
+                m,r = divmod(m,2)
+                result.append(r)
+            self.bits = result[::-1]
+            self.n = n
+        elif n == None and bits != None:
+            self.bits = bits.copy()
+            self.n = 0
+            for i in self.bits:
+                self.n *= 2
+                self.n += i
+        else:
+            raise ValueError('Need either n or bits, but not both')
 
     def __str__(self):
         return str(self.bits)
@@ -71,13 +91,10 @@ class State:
     def __len__(self):
         return len(self.bits)
 
-def convert(state):
-    result = 0
-    for i in state:
-        result = 2 * result + i
-    return result
+    def __getitem__(self,key):
+        return self.bits[key]
 
-def project(state,table=[0,1,1,0]):
+class Projection:
     '''
     Project a state using a function defined by a table.
 
@@ -85,22 +102,29 @@ def project(state,table=[0,1,1,0]):
         state an array represening pairs of states
         table A four element of a list containing projection of (0,0), (0,1), etnc
     '''
-    return [table[2*state[i]+state[i+1]] for i in range(0,len(state),2)]
+    def __init__(self,table=[0,1,1,0]):
+        self.table = table
+
+    def __getitem__(self,key):
+        if len(key) % 2 != 0: raise ValueError('key should have even length')
+        return [self.table[2*key[i]+key[i+1]] for i in range(0,len(key),2)]
+
+### old code ###
+def convert(state):
+    result = 0
+    for i in state:
+        result = 2 * result + i
+    return result
+
+
 
 if __name__=='__main__':
     r105 = Rule(105)
     print (r105)
     for i in range(8):
-        state = State(i)
-        print (state,  r105[state], r105.execute(state))
+        state = State(n=i)
+        print (state,  r105[state])
+    P = Projection()
+    print(P[0,0,0,1,1,0,1,1])
+    r105.two_step([0,1,1,0,0,1])
 
-
-
-    # print(execute_rule([0,0,0],r105))
-    #print (create_rule(110))
-    #print (create_rule(137))
-    # r = create_rule(28)
-    # state = [1]
-    # for i in range(25):
-        # state = execute_rule(state,r)
-        # print (convert(state))
